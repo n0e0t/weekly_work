db4bd15b64f2182d98d42839a0e05007d6391239
n0e0t <pongsapadnet@gmail.com>
2022-08-22 18:22:39 +0700

    week3

---
+++ week3/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("enter number :");
+    scanf("%d",&x);
+    for (int i = 0; i < x; i++)
+    {
+       printf("*");
+    }
+    
+}
---
+++ week3/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x,z=0;
+    printf("enter number :");
+    scanf("%d",&x);
+    while (x>z)
+    {
+       printf("*");
+       z+=1;
+    }
+    
+}
